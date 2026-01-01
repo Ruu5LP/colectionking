83c85250e5d691f97021994b3d97970972004a73
Ruu5LP <116436229+Ruu5LP@users.noreply.github.com>
2026-01-01 20:05:54 +0900

    fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Vite -->
+        @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     </head>
     <body class="antialiased">
